0d06c2676ee85b76a298fde613dd43ad5010e4f5
IgorOsavoluk <36710349+BattleHoundCat@users.noreply.github.com>
2021-03-08 13:04:35 +0400

    Add files via upload

---
+++ MultiThreadApp/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace MultiThreadApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWeatherProcess = new System.Windows.Forms.Button();
+            this.btnTimerProcess = new System.Windows.Forms.Button();
+            this.btnParseTextProcess = new System.Windows.Forms.Button();
+            this.btnCloseAllTasks = new System.Windows.Forms.Button();
+            this.lblSumTasks = new System.Windows.Forms.Label();
+            this.txtBoxWeather = new System.Windows.Forms.TextBox();
+            this.txtBoxTimer = new System.Windows.Forms.TextBox();
+            this.txtBoxText = new System.Windows.Forms.TextBox();
+            this.txtBoxLogging = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnWeatherProcess
+            // 
+            this.btnWeatherProcess.Location = new System.Drawing.Point(62, 73);
+            this.btnWeatherProcess.Name = "btnWeatherProcess";
+            this.btnWeatherProcess.Size = new System.Drawing.Size(121, 28);
+            this.btnWeatherProcess.TabIndex = 0;
+            this.btnWeatherProcess.Text = "Get Weather";
+            this.btnWeatherProcess.UseVisualStyleBackColor = true;
+            this.btnWeatherProcess.Click += new System.EventHandler(this.btnWeatherProcess_Click);
+            // 
+            // btnTimerProcess
+            // 
+            this.btnTimerProcess.Location = new System.Drawing.Point(62, 107);
+            this.btnTimerProcess.Name = "btnTimerProcess";
+            this.btnTimerProcess.Size = new System.Drawing.Size(121, 28);
+            this.btnTimerProcess.TabIndex = 1;
+            this.btnTimerProcess.Text = "Run Timer";
+            this.btnTimerProcess.UseVisualStyleBackColor = true;
+            this.btnTimerProcess.Click += new System.EventHandler(this.btnTimerProcess_Click);
+            // 
+            // btnParseTextProcess
+            // 
+            this.btnParseTextProcess.Location = new System.Drawing.Point(62, 141);
+            this.btnParseTextProcess.Name = "btnParseTextProcess";
+            this.btnParseTextProcess.Size = new System.Drawing.Size(121, 28);
+            this.btnParseTextProcess.TabIndex = 2;
+            this.btnParseTextProcess.Text = "Run Parse Text";
+            this.btnParseTextProcess.UseVisualStyleBackColor = true;
+            this.btnParseTextProcess.Click += new System.EventHandler(this.btnParseTextProcess_Click);
+            // 
+            // btnCloseAllTasks
+            // 
+            this.btnCloseAllTasks.Location = new System.Drawing.Point(62, 243);
+            this.btnCloseAllTasks.Name = "btnCloseAllTasks";
+            this.btnCloseAllTasks.Size = new System.Drawing.Size(161, 54);
+            this.btnCloseAllTasks.TabIndex = 4;
+            this.btnCloseAllTasks.Text = "Close All Tasks";
+            this.btnCloseAllTasks.UseVisualStyleBackColor = true;
+            this.btnCloseAllTasks.Click += new System.EventHandler(this.btnCloseAllTasks_Click);
+            // 
+            // lblSumTasks
+            // 
+            this.lblSumTasks.AutoSize = true;
+            this.lblSumTasks.Location = new System.Drawing.Point(59, 42);
+            this.lblSumTasks.Name = "lblSumTasks";
+            this.lblSumTasks.Size = new System.Drawing.Size(108, 13);
+            this.lblSumTasks.TabIndex = 5;
+            this.lblSumTasks.Text = "Sum Active Tasks : 0";
+            // 
+            // txtBoxWeather
+            // 
+            this.txtBoxWeather.Location = new System.Drawing.Point(308, 73);
+            this.txtBoxWeather.Name = "txtBoxWeather";
+            this.txtBoxWeather.Size = new System.Drawing.Size(264, 20);
+            this.txtBoxWeather.TabIndex = 6;
+            // 
+            // txtBoxTimer
+            // 
+            this.txtBoxTimer.Location = new System.Drawing.Point(308, 107);
+            this.txtBoxTimer.Name = "txtBoxTimer";
+            this.txtBoxTimer.Size = new System.Drawing.Size(264, 20);
+            this.txtBoxTimer.TabIndex = 7;
+            // 
+            // txtBoxText
+            // 
+            this.txtBoxText.Location = new System.Drawing.Point(308, 141);
+            this.txtBoxText.Multiline = true;
+            this.txtBoxText.Name = "txtBoxText";
+            this.txtBoxText.Size = new System.Drawing.Size(264, 53);
+            this.txtBoxText.TabIndex = 8;
+            // 
+            // txtBoxLogging
+            // 
+            this.txtBoxLogging.Location = new System.Drawing.Point(308, 200);
+            this.txtBoxLogging.Name = "txtBoxLogging";
+            this.txtBoxLogging.Size = new System.Drawing.Size(264, 20);
+            this.txtBoxLogging.TabIndex = 9;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 309);
+            this.Controls.Add(this.txtBoxLogging);
+            this.Controls.Add(this.txtBoxText);
+            this.Controls.Add(this.txtBoxTimer);
+            this.Controls.Add(this.txtBoxWeather);
+            this.Controls.Add(this.lblSumTasks);
+            this.Controls.Add(this.btnCloseAllTasks);
+            this.Controls.Add(this.btnParseTextProcess);
+            this.Controls.Add(this.btnTimerProcess);
+            this.Controls.Add(this.btnWeatherProcess);
+            this.Name = "MainForm";
+            this.Text = "MultiThread App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWeatherProcess;
+        private System.Windows.Forms.Button btnTimerProcess;
+        private System.Windows.Forms.Button btnParseTextProcess;
+        private System.Windows.Forms.Button btnCloseAllTasks;
+        private System.Windows.Forms.Label lblSumTasks;
+        private System.Windows.Forms.TextBox txtBoxWeather;
+        private System.Windows.Forms.TextBox txtBoxTimer;
+        private System.Windows.Forms.TextBox txtBoxText;
+        private System.Windows.Forms.TextBox txtBoxLogging;
+    }
+}
+
---
+++ MultiThreadApp/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Runtime.Remoting.Contexts;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace MultiThreadApp
+{
+    public partial class MainForm : Form
+    {
+        public class TemperatureInfo
+        {
+            public float Temp { get; set; }
+        }
+        public class WeatherResponse
+        {
+            public TemperatureInfo Main { get; set; }
+
+            public string Name { get; set; }
+
+            
+        }
+        public int ActiveTasks = 0;
+
+        static public DateTime currentTime = DateTime.Now;
+
+        string FullBook;
+
+        string[] words;
+
+        static int indexCounter = 0;
+        List<System.Windows.Forms.Timer> timerlist = new List<System.Windows.Forms.Timer>();
+
+        string targetPath = @"E:\Test\testlog.txt";
+
+        public MainForm()
+        {
+            InitializeComponent();
+            lblSumTasks.Text = $"All Active Tasks: {ActiveTasks}";
+            System.Windows.Forms.Timer BasicTimer = new System.Windows.Forms.Timer();
+            System.Windows.Forms.Timer WeatherTimer = new System.Windows.Forms.Timer();
+            System.Windows.Forms.Timer TextTimer = new System.Windows.Forms.Timer();
+            timerlist.Add(BasicTimer);
+            timerlist.Add(WeatherTimer);
+            timerlist.Add(TextTimer);
+
+        }
+
+        private void btnWeatherProcess_Click(object sender, EventArgs e)
+        {
+            //GetWeather();
+            timerlist[1].Tick += new EventHandler(GetWeather);
+            timerlist[1].Interval = 4000;
+            timerlist[1].Start();
+            btnWeatherProcess.Enabled = false;
+            UpActivePTasks();
+
+        }
+        private void GetWeather(object sender, EventArgs e)
+        {
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=Moscow,Ru&units=metric&appid=51d01be8c740b9dfd598d0d246ec6adb";
+
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+            string response;
+
+            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                response = streamReader.ReadToEnd();
+            }
+
+            WeatherResponse weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(response);
+
+            txtBoxWeather.Text = $"Temperature in {weatherResponse.Name}: {weatherResponse.Main.Temp} °C";
+        }
+
+        private void btnTimerProcess_Click(object sender, EventArgs e)
+        {
+
+            txtBoxTimer.Text = currentTime.ToString();
+            btnTimerProcess.Enabled = false;
+            timerlist[0].Tick += new EventHandler(TimerTick);
+            timerlist[0].Interval = 5000;
+            timerlist[0].Start();
+            UpActivePTasks();
+
+        }
+
+        private void btnParseTextProcess_Click(object sender, EventArgs e)
+        {
+            ParseBook();
+            timerlist[2].Tick += new EventHandler(InsertTextBlock);
+            timerlist[2].Interval = 4000;
+            timerlist[2].Start();
+            btnParseTextProcess.Enabled = false;
+            UpActivePTasks();
+
+        }
+
+        private void btnWriteLogProcess_Click(object sender, EventArgs e)
+        {
+            UpActivePTasks();
+        }
+
+        private void btnCloseAllTasks_Click(object sender, EventArgs e)
+        {
+            StopActiveTasks();
+            btnCloseAllTasks.Enabled = false;
+            this.Close();
+
+        }
+
+        private void UpActivePTasks()
+        {
+            ActiveTasks++;
+            lblSumTasks.Text = $"All Active Tasks: {ActiveTasks}";
+        }
+        private void StopActiveTasks()
+        {
+            ActiveTasks = 0;
+            foreach ( var mytimer in timerlist)
+            {
+                mytimer.Stop();
+            }
+            lblSumTasks.Text = $"All Active Tasks: {ActiveTasks}";
+        }
+        private void TimerTick(object sender, EventArgs e)
+        {
+            txtBoxTimer.Text = DateTime.Now.ToString();
+        }
+        private void ParseBook() 
+        {
+            WebClient wc = new WebClient();
+
+            wc.DownloadStringCompleted += (s, eArgs) =>
+            {
+                s = eArgs.Result;
+                this.FullBook = (string)s;
+                txtBoxText.Text = "Книга загружена";
+                words = FullBook.Split(' ');
+            };
+
+            //  Загрузить  электронную  книгу  из Библиотеки Гутенберга.
+
+            wc.DownloadStringAsync(new Uri("http://www.gutenberg.org/files/64630/64630-0.txt"));
+        }
+        /// <summary>
+        /// Вставить блок текста раз в определенный интервал времени
+        /// </summary>
+        private void InsertTextBlock(object sender, EventArgs e)
+        {
+            txtBoxText.Text = "";
+            txtBoxLogging.Text = "Готовится вставка новых данных";
+            var query = from word in words
+                        .Skip(indexCounter)
+                        .Take(20)
+                        select word;
+            foreach(var i in query)
+            {
+                txtBoxText.Text += i.ToString();
+                txtBoxText.Text += ' ';
+            }
+            indexCounter += 20;
+            timerlist[2].Stop();
+            Application.DoEvents();
+            txtBoxLogging.Text = "Новые слова получены";
+            Thread.Sleep(1500);               
+            //запустить лог в файл
+            LogIntoFile(txtBoxText.Text);
+
+        }
+        private async void LogIntoFile(string inputWords)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(targetPath, append: true, System.Text.Encoding.Default))
+                {
+                    await sw.WriteLineAsync(inputWords);
+                }
+                txtBoxLogging.Text = "Запись выполнена";
+            }
+            catch (Exception e)
+            {
+                txtBoxLogging.Text = e.Message;
+            }
+            timerlist[2].Start();
+        }
+    }
+}
